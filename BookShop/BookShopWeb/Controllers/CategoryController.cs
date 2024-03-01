0884b5a4a4356be0bce9fcf18c7f8365e7b60eef
Ksenia Antilevskaya <antikevkun@gmail.com>
2024-03-01 14:15:41 +0300

    Create CategoryController & add categories in database

---
+++ BookShop/BookShopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BookShopWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        public CategoryController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var categoriesList = _dbContext.Categories.ToList();
+            return View(categoriesList);
+        }
+    }
+}
