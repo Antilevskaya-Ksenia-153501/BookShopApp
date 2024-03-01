334690dc557913e7e2d0ca7017015b75deaf9a9e
Ksenia Antilevskaya <antikevkun@gmail.com>
2024-03-01 11:09:53 +0300

    Create ASP.NET MVC app & add class for category

---
+++ BookShop/BookShopWeb/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShopWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
